0881785eab81ff207ae13b4db34c2226b3fd01b0
Elena <ebogacheva@yahoo.com>
2022-09-29 01:05:40 +0300

    changes

--- src/main/java/homework1/task2/LinearEquation.java
+++ src/main/java/homework1/task2/LinearEquation.java
@@ -16,10 +16,12 @@ public class LinearEquation {
 
         if (a != 0) {
             System.out.println("Result: " + (-b) / a);
-        } else if (a == 0 && b != 0) {
-            System.out.println("The equation has no roots");
-        } else if (a == 0 && b == 0) {
-            System.out.println("The root of the equation is any number");
+        } else {
+            if (b != 0) {
+                System.out.println("The equation has no roots");
+            } else {
+                System.out.println("The root of the equation is any number");
+            }
         }
     }
 }
