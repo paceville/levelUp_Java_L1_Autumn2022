b90557ba809548b3da644ef188a5999e2c791163
Elena <ebogacheva@yahoo.com>
2022-10-03 19:07:29 +0300

    Task6 update

--- src/main/java/org/levelup/homework2/Task6.java
+++ src/main/java/org/levelup/homework2/Task6.java
@@ -4,12 +4,11 @@ public class Task6 {
     public static void main(String[] args) {
 
         int number = 997;
-        String s = String.valueOf(number);
-        while (s.length() <= 4) {
-            number += 3;
-            s = String.valueOf(number);
 
-            if (s.length() > 4) {
+        while (true) {
+            number += 3;
+
+            if (number / 1000 == 10) {
                 break;
             }
 
